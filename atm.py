6c3e9bfde4370a38d0dc36085466e6cf0aad0302
jisoo <wltnquf98@gmail.com>
2025-01-09 17:09:17 +0900

    🐛 입력 검증 및 에러 처리 추가
    잘못 입력된 값을 처리하도록 기능 추가

--- atm.py
+++ atm.py
@@ -1,10 +1,6 @@
-
-#출금 요청한 금액을 받는 변수 : withdraw_amount
-#출금을 요청한 금액을 balance 변수에서 뺀 결과가 들어가도록 코드를 작성해주세요
-#영수증에 다음 순서로 값이 들어가도록 코드를 만들어 주세요 -> "출금", withdraw_amount,balance) 순으로 데이터를 넣어주세요
-#가지고 있는 금액보다 출금을 원하는 금액이 클때 가지고 있는 금액만 출금 되도록 코드를 작성해주세요
-#단, 영수증에 내역은 변경되지 않아야 하며 입금 또는 출금이 진행될때마다 이력이 기록됩니다.
-#영수증 변수는 : receipts
+#입력 검증 및 에러 처리 추가
+#잘못된 입력 값 (숫자가 아닌값,음수 값 등)
+#유효하지 않은 메뉴 선택 시 경고 메시지 또는 사용방법 재안내를 해주세요 
 receipts = []  # []대괄호 : list / {key:value}중괄호 : dict / () : tuple
 balance = 3000
 withdraw_amount =[]
@@ -14,10 +10,13 @@ while True:
     if num == '4':
         break
     if num == '1':
-        deposit_amount=int(input('입금할 금액을 입력해주세요 : '))        #input()=내장함수  int()=정수형으로 변환
-        balance += deposit_amount  #balance = balance + deposit_amount
-        receipts.append(("입금", deposit_amount, balance))
-        print(f'입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+        deposit_amount= input('입금할 금액을 입력해주세요 : ')        
+        if deposit_amount.isdigit() and int(deposit_amount) > 0: #1000 ->  True 천원 -> False
+            balance += int(deposit_amount)  #balance = balance + deposit_amount
+            receipts.append(("입금", deposit_amount, balance))
+            print(f'입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+        else:
+            print('입금한 금액을 숫자 형태와 음수가 아닌 값을 입력해주세요')
     if num == '2':
         withdraw_amount=int(input('출금하실 금액을 입력해주세요 :'))
         withdraw_amount=min(balance,withdraw_amount)
---
+++ test.py
@@ -0,0 +1,2 @@
+num = int(input('값을 넣어주세요'))
+print(num)
